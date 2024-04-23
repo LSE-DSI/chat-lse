f30587c2b4de43b1a7d10ccb63a6c1063c1f73e5
Riya Chhikara <r.chhikara@lse.ac.uk>
2024-04-23 07:03:52 +0100

    updated the project

--- lse_dsi_scrape/lse_dsi_scrape/spiders/crawler.py
+++ lse_dsi_scrape/lse_dsi_scrape/spiders/crawler.py
@@ -1,30 +1,43 @@
 import scrapy
-import pandas as pd
+import os 
 
 class MySpider(scrapy.Spider):
     name = 'dsi_scraper'
-
-    def start_requests(self):
-        urls = [
-            'https://www.lse.ac.uk/DSI',
-        ]
-        
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'https://www.lse.ac.uk/DSI',
+    ]
 
     def parse(self, response):
-        data = []
-        for link in response.css("a.component__link"):
-            title = link.css("h2.component__title ::text").get().strip()
-            text = ' '.join(link.css(".component__details ::text").getall()).strip()
-            url = link.attrib['href']
-            data.append({'Title': title, 'Text': text, 'URL': url})
+        # Extract data from the current page
+        for box in response.css("a.component__link"):
+            yield {
+                'Title': box.css("h2.component__title ::text").get().strip(),
+                'Text': ' '.join(box.css(".component__details ::text").getall()).strip(),
+                'URL': box.attrib['href']
+            }
 
-        # Creating DataFrame
-        df = pd.DataFrame(data)
+        # Follow links found on the current page
+        for next_page_url in response.css("a.component__link::attr(href)").extract():
+            yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse_linked_page)
 
-        # Saving to CSV
-        df.to_csv('scrapy_data.csv')
-
-        self.log('Saved data to scrapy_data.csv')
-
+    def parse_linked_page(self, response):
+        # Extract data from the linked page
+        page = {
+            'Title': response.css('title::text').get(),
+            'Text': ' '.join(response.css('p::text').getall()),
+            'URL': response.url
+        }
+        
+        # Create a folder if it doesn't exist
+        folder_name = 'linked_pages'
+        os.makedirs(folder_name, exist_ok=True)
+        
+        filename = f"{folder_name}/Linked_DSI_Page_{response.url.split('/')[-1]}.txt"
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(f"Title: {page['Title']}\n")
+            f.write(f"Text: {page['Text']}\n")
+            f.write(f"URL: {page['URL']}\n")
+        
+        self.log(f"Saved file {filename}")
+        
+        yield page
