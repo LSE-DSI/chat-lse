d9975e64d95c0536a5bf245acc4919c228a5be35
Riya Chhikara <r.chhikara@lse.ac.uk>
2024-06-07 20:11:40 +0100

    Added functionality for higher scraping depth.

--- dsi_crawler/spiders/crawler.py
+++ dsi_crawler/spiders/crawler.py
@@ -1,5 +1,4 @@
 import scrapy
-
 from dsi_crawler.items import DSIPagesScraperItem
 from dsi_crawler.items import BoxScraperItem
 
@@ -9,13 +8,51 @@ class SpiderDSI(scrapy.Spider):
     start_urls = [
         'https://www.lse.ac.uk/DSI',
     ]
+    max_depth = 3
 
     def parse(self, response):
-        # Extract data from the current page
-        for box in response.css("a.component__link"):
+        # Extract box data from the current page
+        self.parse_boxes(response)
 
+        # Follow links found on the current page
+        for next_page_url in response.css("a.component__link::attr(href)").extract():
+            yield scrapy.Request(
+                response.urljoin(next_page_url),
+                callback=self.parse_linked_page,
+                meta={'depth': response.meta.get(
+                    'depth', 0) + 1, 'origin_url': response.url}
+            )
+
+    def parse_linked_page(self, response):
+        # Extract data from the linked page
+        item = DSIPagesScraperItem()
+        item['origin_url'] = response.meta.get(
+            'origin_url', self.start_urls[0])
+        item['url'] = response.url
+        item['title'] = response.css('title::text').get().strip()
+        item['html'] = response.text
+        item['date_scraped'] = response.headers['Date'].decode()
+
+        yield item
+
+        # Extract box data from the linked page
+        self.parse_boxes(response)
+
+        # Follow links found on the linked page if the depth is less than max_depth
+        current_depth = response.meta.get('depth', 1)
+        if current_depth < self.max_depth:
+            for next_page_url in response.css("a.component__link::attr(href)").extract():
+                yield scrapy.Request(
+                    response.urljoin(next_page_url),
+                    callback=self.parse_linked_page,
+                    meta={'depth': current_depth + 1,
+                          'origin_url': response.url}
+                )
+
+    def parse_boxes(self, response):
+        for box in response.css("a.component__link"):
             item = BoxScraperItem()
-            item['origin_url'] = self.start_urls[0]
+            item['origin_url'] = response.url
             item['url'] = box.attrib['href']
             item['title'] = box.css("h2.component__title ::text").get().strip()
             item['html'] = '\n'.join(
@@ -27,19 +64,3 @@ class SpiderDSI(scrapy.Spider):
                 ".component__img img::attr(alt)").get()
 
             yield item
-
-        # Follow links found on the current page
-        for next_page_url in response.css("a.component__link::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse_linked_page)
-
-    def parse_linked_page(self, response):
-        # Extract data from the linked page
-
-        item = DSIPagesScraperItem()
-        item['origin_url'] = self.start_urls[0]
-        item['url'] = response.url
-        item['title'] = response.css('title::text').get().strip()
-        item['html'] = response.text
-        item['date_scraped'] = response.headers['Date'].decode()
-
-        yield item
