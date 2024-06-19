e7be59f9b21c9e8006ea6d391b1fea655adcccbf
KristinaD1910 <K.Dixon1@lse.ac.uk>
2024-06-19 09:48:49 +0100

    initial setup

--- setup_duckdb_seeddata.py
+++
@@ -1,5 +0,0 @@
-from setup_duckdb_database.py import create_table, insert_document
-
-create_table()
-insert_document(1, 'Document 1', 'This is the content of document 1', 'vector1')
-insert_document(2, 'Document 2', 'This is the content of document 2', 'vector2')
