2e208793d14076e17d96ac0ef158dfb28b0cf15e
Jon Cardoso-Silva <jonathan.car.silva@gmail.com>
2024-07-04 14:36:12 +0100

    Recreate data/files if it does not exist

--- crawler/spiders/file_downloader.py
+++ crawler/spiders/file_downloader.py
@@ -3,6 +3,8 @@ import os
 from crawler.items import FilesScraperItem
 from dateutil.parser import parse
 
+DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'data', 'files')
+
 class FileDownloaderSpider(scrapy.Spider):
     name = 'file_downloader'
     start_urls = [
@@ -10,13 +12,14 @@ class FileDownloaderSpider(scrapy.Spider):
          ]
     
     def parse(self, response):
+
         for file_link in response.css("a::attr(href)").extract():
             if file_link.endswith(('.pdf', '.doc', '.docx', '.ppt', '.pptx')):
                 # Save metadata for the files to be downloaded 
                 item = FilesScraperItem()
                 item["url"] = file_link 
                 item["title"] = file_link.split('/')[-1]
-                item["file_path"] = os.path.join('data/files/', item["title"])
+                item["file_path"] = os.path.join(DATA_FOLDER, item["title"])
                 item["date_scraped"] = item['date_scraped'] = self.parse_as_datetime(response.headers['Date'].decode())
 
                 yield item 
@@ -27,8 +30,10 @@ class FileDownloaderSpider(scrapy.Spider):
     def save_file(self, response):
         file_name = response.url.split('/')[-1]
         
+        os.makedirs(DATA_FOLDER, exist_ok=True)
+
         # Save the file
-        file_path = os.path.join('data/files/', file_name)
+        file_path = os.path.join(DATA_FOLDER, file_name)
         with open(file_path, 'wb') as f:
             f.write(response.body)
         self.log(f'Saved file {file_name}')
