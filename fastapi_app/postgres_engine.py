80bcdb9e00645b4f81fd03b7d49fa267781bf80e
Jinshuai Ma <mayazureee@gmail.com>
2024-07-04 17:16:03 +0100

    Add configurable Postgres port number.

--- fastapi_app/postgres_engine.py
+++ fastapi_app/postgres_engine.py
@@ -7,11 +7,11 @@ from sqlalchemy import Engine, create_engine
 logger = logging.getLogger("ragapp")
 
 
-async def create_postgres_engine(*, host, username, database, password, sslmode) -> AsyncEngine:
+async def create_postgres_engine(*, host, port, username, database, password, sslmode) -> AsyncEngine:
     
     logger.info("Authenticating to PostgreSQL using password...")
 
-    DATABASE_URI = f"postgresql+asyncpg://{username}:{password}@{host}/{database}"
+    DATABASE_URI = f"postgresql+asyncpg://{username}:{password}@{host}:{port}/{database}"
     # Specify SSL mode if needed
     if sslmode:
         DATABASE_URI += f"?ssl={sslmode}"
@@ -28,6 +28,7 @@ async def create_postgres_engine_from_env() -> AsyncEngine:
 
     engine = await create_postgres_engine(
         host=os.environ["POSTGRES_HOST"],
+        port=os.environ.get("POSTGRES_PORT", 5432),
         username=os.environ["POSTGRES_USERNAME"],
         database=os.environ["POSTGRES_DATABASE"],
         password=os.environ["POSTGRES_PASSWORD"],
@@ -37,11 +38,11 @@ async def create_postgres_engine_from_env() -> AsyncEngine:
     return engine
 
 
-def create_postgres_engine_sync(*, host, username, database, password, sslmode) -> Engine:
+def create_postgres_engine_sync(*, host, port, username, database, password, sslmode) -> Engine:
     
     logger.info("Authenticating to PostgreSQL using password...")
 
-    DATABASE_URI = f"postgresql+psycopg2://{username}:{password}@{host}/{database}"
+    DATABASE_URI = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}"
     # Specify SSL mode if needed
     # if sslmode:
     #     DATABASE_URI += f"?ssl={sslmode}"
@@ -58,6 +59,7 @@ def create_postgres_engine_from_env_sync() -> Engine:
 
     engine = create_postgres_engine_sync(
         host=os.environ["POSTGRES_HOST"],
+        port=os.environ.get("POSTGRES_PORT", 5432),
         username=os.environ["POSTGRES_USERNAME"],
         database=os.environ["POSTGRES_DATABASE"],
         password=os.environ["POSTGRES_PASSWORD"],
