a497ce3ba9ee48dab7d201168cbbff23012a7ecf
tz1211 <terryzhou1211@gmail.com>
2024-07-11 11:31:22 +0100

    create chatlse package for all app functions

---
+++ chatlse/clients.py
@@ -0,0 +1,25 @@
+import os
+import openai
+import logging
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+logger = logging.getLogger("ragapp")
+
+DEFAULT_EMBED_MODEL = "thenlper/gte-large"
+
+
+async def create_chat_client():
+    logger.info("Authenticating to OpenAI using Ollama...") 
+    chat_client = openai.AsyncOpenAI(
+        base_url=os.getenv("OLLAMA_ENDPOINT"),
+        api_key="nokeyneeded",
+    )
+    chat_model = os.getenv("OLLAMA_CHAT_MODEL")
+    
+    return chat_client, chat_model
+
+
+async def create_embed_client():
+    logger.info("Authenticating to OpenAI using Ollama...") 
+    embed_model = HuggingFaceEmbedding(model_name=os.getenv("OLLAMA_EMBED_MODEL", DEFAULT_EMBED_MODEL))
+    return embed_model
