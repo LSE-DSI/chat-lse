8f1a08dd00a0034e44fec27615b4556602226471
tz1211 <terryzhou1211@gmail.com>
2024-08-30 14:55:12 +0800

    refactor rag_advanced to have QueryRewriterRAG as a subclasss of AdvancedRAGChat

--- src/python/chatlse/llm_functions.py
+++ src/python/chatlse/llm_functions.py
@@ -31,109 +31,71 @@ def extract_json(chat_response: ChatCompletion):
 
 
 
-def build_filter_function(model=None) -> list[ChatCompletionToolParam]:
-    if model == "llama3.1:8b-instruct-q8_0": # Function tools for Llama model
-        return [
-            {
-            "type": "function",
-            "function": {
-                "name": "filter_queries",
-                "description": '''Decide whether the model should answer a user query by judging whether it is in scope.
-                        Respond in the format: {"is_greeting": true/false, "is_follow_up": true/false, "is_reference": true/false, "is_relevant": true/false, "requires_clarification": true/false, "is_farewell": true/false}
-                        Do NOT enclose the true or false values in quotes.''',
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "requires_clarification":{
-                            "type": "boolean",
-                            "description": '''Based ONLY on the last user query, decide if answering this question requires further clarification in order to provide a precise answer. For example, whether the student is a postgraduate or undergraduate. Only answer true if the question is ambiguous or unclear.
-                            E.g
-                            {"User": "How can I apply for postrgraduate courses at LSE", 
-                                "requires_clarification": true}''',
-                        },
-                        "is_greeting": {
-                            "type": "boolean",
-                            "description": "Based ONLY on the last user query, decide if the query is a greeting, e.g. Hi, Hello, How are you, What's up, Sup, thanks, thank you, bye, great etc.",
-                        },
-                        "is_follow_up": {
-                            "type": "boolean",
-                            "description": '''Based on the last user query and the previous model response (if provided), decide if the user query references or follows up on the model's previous response.
-                            Example: 
-                            {
-                                "model response": "LSE offers courses and modules related to AI, such as ST449 Artificial Intelligence, which covers topics including simple and advanced search algorithms, gameplay, constraint satisfaction problems (CSP), knowledge representation, supervised learning, and reinforcement learning.",
-                                "user query": "Tell me more about ST449", 
-                                "is_follow_up": ture
-                            }
-                            ''',
-                        },
-                        "is_reference": {
-                            "type": "boolean",
-                            "description": "Based on the last user query and the previous model response (if provided), does the user query mention anything in the previous model response?",
-                        },
-                        "is_relevant": {
-                            "type": "boolean",
-                            "description": "You are an assistant at the London School fo Economics (LSE). Your job is to answer any administrative questions that the staff and students may have. You must also handle all mental health related queries. Based on the last user query and the previous model response (if provided), decide if you should answer the user query provided. If the statement is purely conversational (e.g. thanks, bye, etc.), it is not relevant. By more aggressive in filtering out irrelevant queries.",
-                        },
+def extract_json_query_rewriter(chat_response: ChatCompletion):
+    to_greet = extract_function_calls(chat_response, "is_greeting") # Judge if query is a greeting 
+    is_relevant = extract_function_calls(chat_response, "is_relevant") # Judge if the query is relevant to the scope of ChatLSE 
+    is_farewell = extract_function_calls(chat_response, "is_farewell") # Judge if the query is a farewell message
 
-                        "is_farewell": {
-                            "type": "boolean",
-                            "description": '''Based ONLY on the last user query, decide if the user has sent a query that suggests they no longer require your help and are leaving the chat.
-                            e.g. 
-                            {"user": "Thank you for your help, goodbye"
-                                is_farewell: true}''',
-                        },
-                    },
-                    "required": ["is_greeting", "is_follow_up", "is_reference", "is_relevant", "requires_clarification", "is_farewell"],
-                },
-            },
-        }     
-    ]
-    else: # Function tools for Mistral (or if model not specified)
-        return [
-            {
-            "type": "function",
-            "function": {
-                "name": "filter_queries",
-                "description": '''Decide whether the model should answer a user query by judging whether it is in scope.
-                        Respond in the format: {"is_greeting": true/false, "is_follow_up": true/false, "is_reference": true/false, "is_relevant": true/false, "requires_clarification": true/false, "is_farewell": true/false}
-                        Do NOT enclose the true or false values in quotes.''',
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "requires_clarification":{
-                            "type": "boolean",
-                            "description": '''Based ONLY on the last user query, decide if answering this question requires further clarification in order to provide a precise answer. For example, whether the student is a postgraduate or undergraduate. Only answer true if the question is ambiguous or unclear.
-                            E.g
-                            {"User": "How can I apply for postrgraduate courses at LSE", 
-                                "requires_clarification": true}''',
-                        },
-                        "is_greeting": {
-                            "type": "boolean",
-                            "description": "Based ONLY on the last user query, decide if the query is a greeting, e.g. Hi, Hello, How are you, What's up, Sup, etc.",
-                        },
-                        "is_follow_up": {
-                            "type": "boolean",
-                            "description": "Based on the last user query and the previous model response (if provided), decide if the user query is a follow up on the model's previous response.",
-                        },
-                        "is_reference": {
-                            "type": "boolean",
-                            "description": "Based on the last user query and the previous model response (if provided), does the user query mention anything in the previous model response?",
-                        },
-                        "is_relevant": {
-                            "type": "boolean",
-                            "description": "You are an assistant at the London School fo Economics (LSE). Your job is to answer any administrative questions that the staff and students may have. You must also handle all mental health related queries. Based on the last user query and the previous model response (if provided), decide if you should answer the user query provided. If the statement is purely conversational (e.g. thanks, bye, etc.), it is not relevant. By more aggressive in filtering out irrelevant queries.",
-                        },
+    return to_greet, is_relevant, is_farewell
 
-                        "is_farewell": {
-                            "type": "boolean",
-                            "description": "Based ONLY on the last user query, decide if the user has sent a query that suggests they no longer require your help and are leaving the chat. e.g. Thank you, Thanks, Bye, Great, etc."
-                        },
+
+
+def build_filter_function() -> list[ChatCompletionToolParam]:
+    return [
+        {
+        "type": "function",
+        "function": {
+            "name": "filter_queries",
+            "description": '''Decide whether the model should answer a user query by judging whether it is in scope.
+                    Respond in the format: {"is_greeting": true/false, "is_follow_up": true/false, "is_reference": true/false, "is_relevant": true/false, "requires_clarification": true/false, "is_farewell": true/false}
+                    Do NOT enclose the true or false values in quotes.''',
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "requires_clarification":{
+                        "type": "boolean",
+                        "description": '''Based ONLY on the last user query, decide if answering this question requires further clarification in order to provide a precise answer. For example, whether the student is a postgraduate or undergraduate. Only answer true if the question is ambiguous or unclear.
+                        E.g
+                        {"User": "How can I apply for postrgraduate courses at LSE", 
+                            "requires_clarification": true}''',
                     },
-                    "required": ["is_greeting", "is_follow_up", "is_reference", "is_relevant", "requires_clarification", "is_farewell"],
+                    "is_greeting": {
+                        "type": "boolean",
+                        "description": "Based ONLY on the last user query, decide if the query is a greeting, e.g. Hi, Hello, How are you, What's up, Sup, thanks, thank you, bye, great etc.",
+                    },
+                    "is_follow_up": {
+                        "type": "boolean",
+                        "description": '''Based on the last user query and the previous model response (if provided), decide if the user query references or follows up on the model's previous response.
+                        Example: 
+                        {
+                            "model response": "LSE offers courses and modules related to AI, such as ST449 Artificial Intelligence, which covers topics including simple and advanced search algorithms, gameplay, constraint satisfaction problems (CSP), knowledge representation, supervised learning, and reinforcement learning.",
+                            "user query": "Tell me more about ST449", 
+                            "is_follow_up": ture
+                        }
+                        ''',
+                    },
+                    "is_reference": {
+                        "type": "boolean",
+                        "description": "Based on the last user query and the previous model response (if provided), does the user query mention anything in the previous model response?",
+                    },
+                    "is_relevant": {
+                        "type": "boolean",
+                        "description": "You are an assistant at the London School fo Economics (LSE). Your job is to answer any administrative questions that the staff and students may have. You must also handle all mental health related queries. Based on the last user query and the previous model response (if provided), decide if you should answer the user query provided. If the statement is purely conversational (e.g. thanks, bye, etc.), it is not relevant. By more aggressive in filtering out irrelevant queries.",
+                    },
+
+                    "is_farewell": {
+                        "type": "boolean",
+                        "description": '''Based ONLY on the last user query, decide if the user has sent a query that suggests they no longer require your help and are leaving the chat.
+                        e.g. 
+                        {"user": "Thank you for your help, goodbye"
+                            is_farewell: true}''',
+                    },
                 },
+                "required": ["is_greeting", "is_follow_up", "is_reference", "is_relevant", "requires_clarification", "is_farewell"],
             },
-        }     
-    ]
+        },
+    }     
+]
 
 
 
@@ -164,3 +126,36 @@ def build_response_function() -> list[ChatCompletionToolParam]:
         }
     ]
 
+
+
+def build_filter_function_query_rewriter() -> list[ChatCompletionToolParam]:
+    return [
+        {
+        "type": "function",
+        "function": {
+            "name": "filter_queries",
+            "description": '''Decide whether the model should answer a user query by judging whether it is in scope.
+                    Respond in the format: {"is_greeting": true/false, "is_relevant": true/false, "is_farewell": true/false}
+                    Do NOT enclose the true or false values in quotes.''',
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "is_greeting": {
+                        "type": "boolean",
+                        "description": "Based ONLY on the last user query, decide if the query is a greeting, e.g. Hi, Hello, How are you, What's up, Sup etc.",
+                    },
+                    "is_relevant": {
+                        "type": "boolean",
+                        "description": "You are an assistant at the London School fo Economics (LSE). Your job is to answer any administrative questions that the staff and students may have. You must also handle all mental health related queries. Based on the last user query and the previous model response (if provided), decide if you should answer the user query provided. If the statement is purely conversational (e.g. thanks, bye, etc.), it is not relevant. If you are unsure, answer true.",
+                    },
+                    "is_farewell": {
+                        "type": "boolean",
+                        "description": '''Based ONLY on the last user query, decide if the user query suggests the termination of the conversation or that they no longer require your help.
+                        E.g. Perfect, Thank you, Thanks, Great, Bye, Goodbye, Thank you for your help, etc.''',
+                    },
+                },
+                "required": ["is_greeting", "is_relevant", "is_farewell"],
+            },
+        },
+    }     
+]
