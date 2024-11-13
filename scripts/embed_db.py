ed01478f8be25d50c9645f03dd0beb9c4d6fd29d
tz1211 <terryzhou1211@gmail.com>
2024-11-13 17:03:51 +0000

    add support for 3 kinds of embedding in embed_db.py

--- scripts/embed_db.py
+++ scripts/embed_db.py
@@ -30,6 +30,11 @@ logging.debug(engine)
 logging.basicConfig(level=logging.INFO)
 
 
+### SET EMBEDDING TYPE HERE 
+embedding_type = "title_embeddings"
+if embedding_type not in ["simple_embeddings", "title_embeddings", "context_embeddings"]: 
+    raise ValueError('embedding_type must be in ["simple_embeddings", "title_embeddings", "context_embeddings"]')
+
 
 # Create a table for document summary 
 with engine.connect() as conn:
@@ -46,37 +51,38 @@ with engine.connect() as conn:
     logging.info("Table doc_summary created successfully.")
 
 
-# Check if the 'embedding' column exists
+# Check if the 'embeddings' column exists
 insp = sqlalchemy.inspect(engine)
 columns = [col['name'] for col in insp.get_columns('lse_doc')]
 
-if 'embedding' not in columns:
+if embedding_type not in columns:
     with engine.connect() as conn:
-        logging.info("Adding 'embedding' column to lse_doc table...")
-        conn.execute(text('''
-            ALTER TABLE lse_doc ADD COLUMN embedding VECTOR(1024);
+        logging.info(f"Adding '{embedding_type}' column to lse_doc table...")
+        conn.execute(text(f'''
+            ALTER TABLE lse_doc ADD COLUMN {embedding_type} VECTOR(1024);
         '''))
 
-        logging.info("'embedding' column added successfully.")
+        logging.info(f"'{embedding_type}' column added successfully.")
         conn.commit() 
 else:
-    logging.info("'embedding' column already exists, no need to add it.")
+    logging.info(f"'{embedding_type}' column already exists, no need to add it.")
 
 
-# Embed documents where embedding is NULL
+# Embed documents where embeddings is NULL
 with engine.connect() as conn:
-    logging.info("Fetching data for embedding calculation...")
-    query = '''
+    logging.info(f"Fetching data for {embedding_type} calculation...")
+    query = f'''
         SELECT id, doc_id, title, content 
         FROM lse_doc 
-        WHERE embedding IS NULL
+        WHERE {embedding_type} IS NULL
     '''
 
     df_results = pd.read_sql_query(query, conn) 
 
     if not df_results.empty: 
         logging.info("Collating full document text...")
-        df_docs = df_results.sort_values("id").groupby("doc_id").agg({"content": lambda x: "".join(x)}).reset_index()
+        if embedding_type == "context_embeddings": 
+            df_docs = df_results.sort_values("id").groupby("doc_id").agg({"content": lambda x: "".join(x)}).reset_index()
 
         total = len(df_results)
         count = 0 
@@ -85,40 +91,47 @@ with engine.connect() as conn:
             count += 1
             id, doc_id, title, chunk_content = row
 
-            logging.info("Summarising document contents...")
-            # Check if the document already exists in the doc_summary
-            summary = conn.execute(
-                text('SELECT summary FROM doc_summary WHERE doc_id = :doc_id'),
-                {'doc_id': doc_id}
-            ).fetchone()
+            if embedding_type == "context_embeddings": 
+                logging.info("Summarising document contents...")
+                # Check if the document already exists in the doc_summary
+                summary = conn.execute(
+                    text('SELECT summary FROM doc_summary WHERE doc_id = :doc_id'),
+                    {'doc_id': doc_id}
+                ).fetchone()
 
-            if not summary: 
-                # Retrieve full document and summarise its content 
-                doc_content = df_docs.loc[df_docs["doc_id"]==doc_id, "content"].iloc[0]
-                summary = summarise_and_embed_sync(doc_content, chat_model_instance=CHAT_MODEL_INSTANCE, embed_model_instance=EMBED_MODEL_INSTANCE)
-                # Insert the document and summary into the table
-                conn.execute(text('''
-                            INSERT INTO doc_summary (doc_id, content, summary)
-                            VALUES (:doc_id, :content, :summary)
-                        '''), {
-                            "doc_id": doc_id,
-                            "content": doc_content,
-                            "summary": summary
-                        })
-                conn.commit()
+                if not summary: 
+                    # Retrieve full document and summarise its content 
+                    doc_content = df_docs.loc[df_docs["doc_id"]==doc_id, "content"].iloc[0]
+                    summary = summarise_and_embed_sync(doc_content, chat_model_instance=CHAT_MODEL_INSTANCE, embed_model_instance=EMBED_MODEL_INSTANCE)
+                    # Insert the document and summary into the table
+                    conn.execute(text('''
+                                INSERT INTO doc_summary (doc_id, content, summary)
+                                VALUES (:doc_id, :content, :summary)
+                            '''), {
+                                "doc_id": doc_id,
+                                "content": doc_content,
+                                "summary": summary
+                            })
+                    conn.commit()
+                else: 
+                    summary = summary[0]
+
+                contextual_chunk = f"title: {title}\nsummary: {summary}\ncontent: {chunk_content}"
+            elif embedding_type == "title_embeddings": 
+                contextual_chunk = f"title: {title}\ncontent: {chunk_content}"
             else: 
-                summary = summary[0]
-
-            logging.info("Embedding contextual chunks...")
-            contextual_chunk = f"title: {title}\nsummary: {summary}\ncontent: {chunk_content}"
-            print(f"CONTEXTUAL CHUNK: {contextual_chunk}")
+                contextual_chunk = chunk_content
+            
+            print(f"CHUNK: {contextual_chunk}")
+            
+            logging.info("Embedding chunks...")
             embedding = compute_text_embedding_sync(contextual_chunk, model_instance=EMBED_MODEL_INSTANCE)
             print("######################################################################")
             
             # Update the table with the calculated embedding
-            conn.execute(text('''
-                UPDATE lse_doc SET embedding = :embedding WHERE id = :id
-            '''), {'embedding': embedding, 'id': id})
+            conn.execute(text(f'''
+                UPDATE lse_doc SET {embedding_type} = :{embedding_type} WHERE id = :id
+            '''), {embedding_type: embedding, 'id': id})
 
             conn.commit()
 
